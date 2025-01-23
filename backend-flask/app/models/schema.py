74e8931cb3ea197ebdc94a714523c41753ce435d
Elliot A <elliot.a@towardthegoalenglish.com>
2025-01-22 19:20:37 -0600

    Successfully loaded demo data from the backend. Testing API. Renamed Lesson component to LessonCard to avoid overloading namespace.

--- backend-flask/app/models/schema.py
+++ backend-flask/app/models/schema.py
@@ -6,8 +6,7 @@ class StudentSchema(Schema):
 
 class LessonSchema(Schema):
     id = fields.Int(dump_only=True)
-    date = fields.Str(required=True)
-    time = fields.Str(required=True)
+    datetime = fields.DateTime(required=True)
     plan = fields.Str(required=True)
     concepts_taught = fields.Str()
     additional_notes = fields.Str()
