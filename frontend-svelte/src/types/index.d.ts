5bc1e0e9bca4f945fb0dbb593945be28c763f32b
Elliot A <elliot.a@towardthegoalenglish.com>
2025-02-12 19:03:32 -0600

    Added API service layer and api wrapper for frontend. Added sample service functions

--- frontend-svelte/src/types/index.d.ts
+++ frontend-svelte/src/types/index.d.ts
@@ -1,6 +1,8 @@
 export interface Student {
   id: number;
   name: string;
+  created_date: string;
+  status: 'active' | 'inactive' | 'hold' | 'trial';
 }
 
 export interface Lesson {
@@ -10,4 +12,32 @@ export interface Lesson {
   concepts_taught: string;
   additional_notes: string;
   students: Student[];
+  created_date: string;
+}
+
+export interface Quiz {
+  id: number;
+  name: string;
+  datetime: string;
+  score: number;
+  notes: string;
+  lesson_id: number;
+  student_id: number;
+}
+
+export interface Level {
+  id: number;
+  student_id: number;
+  start_date: string;
+  level_category: string;
+}
+
+export interface User {
+  id: number;
+  first_name: string;
+  last_name: string;
+  created_date: string;
+  last_login?: string;
+  password: string;
+  role: string;
 }
