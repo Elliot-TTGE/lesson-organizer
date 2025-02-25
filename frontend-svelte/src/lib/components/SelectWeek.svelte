3d0ef664945aabdaaaebb5d090f547778c01c454
Elliot A <elliot.a@towardthegoalenglish.com>
2025-02-25 12:22:52 -0600

    Add SelectWeek component and integrate date selection in the navbar

---
+++ frontend-svelte/src/lib/components/SelectWeek.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let { startDate = $bindable()}: { startDate: Date } = $props();
+
+</script>
+
+<p>Week:</p>
+<input type="date" class="input input-ghost ml-4" value={startDate.toISOString().slice(0, 10)} onblur={(e: Event) => startDate = new Date((e.target as HTMLInputElement).value)}/>
