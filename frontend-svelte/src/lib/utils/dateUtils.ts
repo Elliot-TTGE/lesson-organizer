d1993055e427afd4662f02de28b3ee8d966d1344
Elliot A <elliot.a@towardthegoalenglish.com>
2025-05-22 15:47:50 -0500

    Refactor date initialization in LessonCard.

--- frontend-svelte/src/lib/utils/dateUtils.ts
+++ frontend-svelte/src/lib/utils/dateUtils.ts
@@ -14,3 +14,13 @@ export function isWithinOneWeek(startOfWeek: Date, dateToCompare: Date): boolean
 
     return startOfWeek.toUTCString() === dateToCompareStartOfWeek.toUTCString();
 }
+
+export function initializeDateTimeInput(datetime: string = new Date().toISOString()) {
+    const localDate = new Date(datetime);
+    const offset = localDate.getTimezoneOffset() * 60000; // offset in milliseconds
+    const localISOTime = new Date(localDate.getTime() - offset).toISOString().slice(0, 16);
+    return {
+      date: localISOTime.split('T')[0],
+      time: localISOTime.split('T')[1],
+    };
+  }
