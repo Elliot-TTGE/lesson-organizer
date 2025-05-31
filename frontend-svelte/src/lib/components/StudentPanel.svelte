686627e89f58054913009778a17f65dff3ddab23
Elliot A <elliot.a@towardthegoalenglish.com>
2025-05-30 19:23:27 -0500

    Decently working HTML and CSS of student table.

--- frontend-svelte/src/lib/components/StudentPanel.svelte
+++ frontend-svelte/src/lib/components/StudentPanel.svelte
@@ -1,38 +1,39 @@
 <script lang="ts">
     import type { Student } from "../../types";
 
-    let students: Number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7];
+    let students: Number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7, 1, 2, 3, 4, 5, 6, 7, 8, 9, 1, 2, 3, 4, 5, 6, 7];
 
     function onMount() {
         // Fetch all students from database.
     }
 </script>
 
-<div class="flex sticky top-16 z-10 bg-base-200 h-24">
-    <div class="flex">
-
+<div class="sticky mt-16 z-10 top-16">
+    <div class="flex bg-base-200">
+        <div class="flex">
+            <!-- Options for student search -->
+        </div>
+        <fieldset class="fieldset ml-auto mr-[2rem] w-[25rem] mb-2">
+            <p class="fieldset-legend">Find a student</p>
+            <label class="input input-accent">
+                <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g
+                    stroke-linejoin="round"
+                    stroke-linecap="round"
+                    stroke-width="2.5"
+                    fill="none"
+                    stroke="currentColor"
+                    >
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                    </g>
+                </svg>
+                <input type="search" placeholder="Search"/>
+            </label>
+        </fieldset>
     </div>
-    <fieldset class="fieldset ml-auto mr-[2rem] w-[25rem] mb-2">
-        <p class="fieldset-legend">Find a student</p>
-        <label class="input input-accent">
-            <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <g
-                stroke-linejoin="round"
-                stroke-linecap="round"
-                stroke-width="2.5"
-                fill="none"
-                stroke="currentColor"
-                >
-                    <circle cx="11" cy="11" r="8"></circle>
-                    <path d="m21 21-4.3-4.3"></path>
-                </g>
-            </svg>
-            <input type="search" placeholder="Search"/>
-        </label>
-    </fieldset>
-</div>
-<div class="overflow-x-auto">
-    <table class="table border border-base-200">
+
+    <table class="table border bg-base-200 border-base-200">
         <thead class="sticky z-10 top-32">
             <tr>
                 <th>Name</th>
@@ -43,22 +44,22 @@
             </tr>
         </thead>
     </table>
-    <div class="overflow-y-auto">
-        <table class="table border border-base-200">
-            <tbody>
-                {#each students as student}
-                    <tr>
-                        <td>student.name</td>
-                        <td>student.level</td>
-                        <td>student.nextLesson</td>
-                        <td>student.lastLesson</td>
-                        <td>student.lastQuiz</td>
-                        <td>
-                            <button class="btn btn-primary btn-sm">Edit</button>
-                        </td>
-                    </tr>
-                {/each}
-            </tbody>
-        </table>
-    </div>
+</div>
+<div class="overflow-y-auto">
+    <table class="table border border-base-200">
+        <tbody>
+            {#each students as student}
+                <tr>
+                    <td>student.name</td>
+                    <td>student.level</td>
+                    <td>student.nextLesson</td>
+                    <td>student.lastLesson</td>
+                    <td>student.lastQuiz</td>
+                    <td>
+                        <button class="btn btn-primary btn-sm">Edit</button>
+                    </td>
+                </tr>
+            {/each}
+        </tbody>
+    </table>
 </div>
