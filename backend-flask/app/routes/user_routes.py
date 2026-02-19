11fb5cfbd63d2464948d9fee91ebb85fc050175c
Elliot A <elliot.a@towardthegoalenglish.com>
2026-02-19 17:36:13 -0600

    Commented out create, update, and delete user routes until they can be given the appropriate permissions in future updates.

--- backend-flask/app/routes/user_routes.py
+++ backend-flask/app/routes/user_routes.py
@@ -43,7 +43,7 @@ def get_users():
     schema = UserSchema(many=True)
     return schema.dump(users), 200
 
-@user_bp.route('/users', methods=['POST'])
+#@user_bp.route('/users', methods=['POST']) # Un-comment when updating permissions for users
 @jwt_required()
 @limiter.limit("10 per hour")
 @response_wrapper
@@ -96,7 +96,7 @@ def create_user():
     db.session.commit()
     return schema.dump(user), 201
 
-@user_bp.route('/users/<int:id>', methods=['PUT'])
+#@user_bp.route('/users/<int:id>', methods=['PUT']) # Un-comment when updating permissions for users
 @jwt_required()
 @response_wrapper
 def update_user(id):
@@ -148,7 +148,7 @@ def update_user(id):
     db.session.commit()
     return schema.dump(updated_user), 200
 
-@user_bp.route('/users/<int:id>', methods=['DELETE'])
+#@user_bp.route('/users/<int:id>', methods=['DELETE']) # Un-comment when updating permissions for users
 @jwt_required()
 @limiter.limit("20 per hour")
 @response_wrapper
